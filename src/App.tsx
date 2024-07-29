9b74c00257ff0b8114260dffd11e9247ad920f37
Hanszman <victor.hanszman@hotmail.com>
2024-07-29 09:14:02 -0300

    implementing socket io connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { io } from 'socket.io-client';
 import './App.css';
+const socket = io('http://localhost:3001');
 
 function App() {
+  socket.on('connect', () => {
+    console.log('connected');
+  });
   return (
     <div className="App">
       <header className="App-header">
