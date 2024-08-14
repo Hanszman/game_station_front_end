e3a98e70af76bcf914bc7676ea3f5fe588ab3197
Hanszman <victor.hanszman@hotmail.com>
2024-08-14 09:10:32 -0300

    chat test

--- src/components/layout/chat/Chat.test.tsx
+++ src/components/layout/chat/Chat.test.tsx
@@ -1,36 +1,28 @@
-import { render, waitFor } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import Chat from './Chat';
 import { io } from 'socket.io-client';
 jest.mock('socket.io-client');
 
 describe('Chat Component', () => {
-    // test('should connect to socket on mount and disconnect on unmount', async () => {
-    //     const mockOn = jest.fn();
-    //     const mockDisconnect = jest.fn();
-    //     (io as jest.Mock).mockReturnValue({
-    //         on: mockOn,
-    //         disconnect: mockDisconnect,
-    //     });
-    //     console.log('Rendering Chat component');
-    //     render(<Chat />);
-    //     await waitFor(() => {
-    //         console.log('Checking mockOn');
-    //         expect(mockOn).toHaveBeenCalled();
-    //         expect(mockOn).toHaveBeenCalledWith('connect', expect.any(Function));
-    //     });
-    //     console.log('Simulating unmount');
-    //     render(<div></div>);
-    //     expect(mockDisconnect).toHaveBeenCalled();
-    // });
-    // test('should call socket.on with connect event on mount', () => {
-    //     render(<Chat />);
-    //     expect(io).toHaveBeenCalled();
-    // });
-    // test('should call socket.on with connect event on mount', () => {
-    //     const mockOn = jest.fn();
-    //     (io as jest.Mock).mockReturnValue({ on: mockOn });
-    //     render(<Chat />);
-    //     expect(io).toHaveBeenCalled();
-    //     expect(mockOn).toHaveBeenCalledWith('connect', expect.any(Function));
-    // });
+    let mockSocket: any;
+    beforeEach(() => {
+        console.log(io);
+        mockSocket = io();
+        console.log(mockSocket);
+    });
+    afterEach(() => {
+        cleanup();
+        jest.clearAllMocks();
+    });
+    test('should connect to socket on mount and disconnect on unmount', () => {
+        const { unmount } = render(<Chat/>);
+        expect(mockSocket.on).toHaveBeenCalledWith('connect', expect.any(Function));
+        const connectCallback = mockSocket.on.mock.calls[0][1];
+        connectCallback();
+        const logSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+        expect(logSpy).toHaveBeenCalledWith('connected');
+        unmount();
+        expect(mockSocket.disconnect).toHaveBeenCalled();
+        logSpy.mockRestore();
+    });
 });
