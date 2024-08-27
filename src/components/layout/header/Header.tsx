40ca4f91522b16115242105782747236ffb53299
Hanszman <victor.hanszman@hotmail.com>
2024-08-27 08:40:53 -0300

    updating header css

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,34 +1,44 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { FaHome, FaBars } from 'react-icons/fa';
+import { FaHome, FaGamepad, FaUser, FaBars } from 'react-icons/fa';
+import { BiSolidGame } from 'react-icons/bi';
 import Translation from '../translation/Translation';
 import Button from '../form/button/Button';
 import './Header.scss';
 
 function Header() {
     const { t } = useTranslation();
-    const [stateImg, setStateImg] = useState('logoBlue');
+    const [stateImg, setStateImg] = useState('logoNormal');
     const [showNavList, setShowNavList] = useState(false);
     return (
         <header className='header'>
             <nav className='navbar'>
                 <Link to='/'>
-                    <FaHome
+                    <BiSolidGame
                         className={`imgLogo ${stateImg}`}
-                        onMouseOver={() => setStateImg('logoWhite')}
-                        onMouseOut={() => setStateImg('logoBlue')}
+                        onMouseOver={() => setStateImg('logoHover')}
+                        onMouseOut={() => setStateImg('logoNormal')}
                     />
                 </Link>
                 <ul className={showNavList ? 'list active' : 'list'}>
                     <li className='item'>
-                        <Link className='itemLink' to='/'>{t('Home')}</Link>
+                        <Link className='itemLink' to='/'>
+                            <FaHome className='itemIcon'/>
+                            {t('Home')}
+                        </Link>
                     </li>
                     <li className='item'>
-                        <Link className='itemLink' to='/games'>{t('Games')}</Link>
+                        <Link className='itemLink' to='/games'>
+                            <FaGamepad className='itemIcon'/>
+                            {t('Games')}
+                        </Link>
                     </li>
                     <li className='item'>
-                        <Link className='itemLink' to='/user'>{t('User')}</Link>
+                        <Link className='itemLink' to='/user'>
+                            <FaUser className='itemIcon'/>
+                            {t('User')}
+                        </Link>
                     </li>
                 </ul>
                 <div className='displayFlex'>
