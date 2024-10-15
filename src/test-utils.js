16154422684d07d81a88e3a2dc00a95c5414953e
Hanszman <victor.hanszman@hotmail.com>
2024-10-15 10:28:39 -0300

    fixing providers for testing

--- src/test-utils.js
+++ src/test-utils.js
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react';
 import i18n from './assets/i18n/i18n';
 import { I18nextProvider } from 'react-i18next';
+import { AuthProvider } from './context/AuthContext';
 
-const renderWithTranslation = (ui, options) => {
-  return render(<I18nextProvider i18n={i18n}>{ui}</I18nextProvider>, options);
+const renderWithProviders = (ui, options) => {
+  return render(
+    <AuthProvider>
+      <I18nextProvider i18n={i18n}>
+        {ui}
+      </I18nextProvider>
+    </AuthProvider>,
+    options
+  );
 };
 
 export * from '@testing-library/react';
-export { renderWithTranslation as render };
+export { renderWithProviders as render };
